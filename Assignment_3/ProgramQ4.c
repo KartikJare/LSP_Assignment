4aa0dc8e5523772dec2b97d225ecd538bf79744b
Kartik Jare <kartikgjare@gmail.com>
2025-12-30 15:29:39 +0530

    Update ProgramQ4.c

--- Assignment_3/ProgramQ4.c
+++ Assignment_3/ProgramQ4.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////
 //
-//  File name   : Assigment1Question4.c
+//  File name   : Assigment4Question4.c
 //  Description : Delete All Empty Regular Files
 //                • Accept directory name.
 //                • Delete files with size = 0 bytes (regular files only).
@@ -20,11 +20,11 @@
 int main()
 {
     DIR *dp = NULL;
-    struct dirent * ptr = NULL;
+    struct dirent *ptr = NULL;
     struct stat sobj;
     char DirName[100];
-    char Path[100];
-    int DeleCount = 0;
+    char Path[300];
+    int DeleteCount = 0,iRet = 0;
 
     printf("Enter directory name : \n");
     scanf("%s", DirName);
@@ -32,9 +32,31 @@ int main()
     dp = opendir(DirName);
     if(dp == NULL)
     {
-        printf("%s",strerror(errno));
-        return;
+        printf("%s\n",strerror(errno));
+        return -1;
     }
-    
+
+    while((ptr = readdir(dp)))
+    {
+        iRet = stat(Path,&sobj);
+
+        if(S_ISREG(sobj.st_mode) && sobj.st_size == 0)
+        {
+            if(unlink(Path) == 0)
+            {
+                printf("Deleted empty file : %s\n",ptr->d_name);
+                DeleteCount++;
+            }
+            else
+            {
+                printf("%s\n",strerror(errno));
+            }
+        }
+    }
+
+    printf("Total empty files deleted : %d\n",DeleteCount);
+
+    closedir(dp);
+
     return 0;
 }
