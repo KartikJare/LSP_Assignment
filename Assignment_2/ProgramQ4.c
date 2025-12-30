a75bb184b725e7f9be84da66dd5ae7887e2f9e85
Kartik Jare <kartikgjare@gmail.com>
2025-12-30 12:57:57 +0530

    Update ProgramQ4.c

--- Assignment_2/ProgramQ4.c
+++ Assignment_2/ProgramQ4.c
@@ -18,15 +18,14 @@
 #include<string.h>
 #include<sys/stat.h>
 #include<dirent.h>
-#include<sys/types.h>
 
 int main()
 {
     DIR *dp = NULL;
     char DirName[100];
-    char Path[100];
     struct dirent *ptr = NULL;
     struct stat sobj;
+    int iRet = 0;
     
     printf("Enter the directory name : \n");
     scanf("%s",DirName);
@@ -37,53 +36,39 @@ int main()
         printf("%s\n",strerror(errno));
         return -1;
     }
-    else
+
+    iRet = lstat(DirName,&sobj);
+
+    printf("File type is :");
+
+    if(S_ISBLK(sobj.st_mode))
     {
-        printf("Directory gets succesfully opened\n");
+        printf("Block Device\n");
     }
-
-    while((ptr = readdir(dp)) != NULL)
+    else if(S_ISCHR(sobj.st_mode))
     {
-
-        snprintf(Path, sizeof(Path), "%s/%s", DirName, ptr->d_name);
-
-        if(lstat(Path, &sobj) == -1)
-        {
-            perror("lstat failed");
-            return;
-        }
-
-        printf("File type is :");
-
-        if(S_ISBLK(sobj.st_mode))
-        {
-            printf("Block Device\n");
-        }
-        else if(S_ISCHR(sobj.st_mode))
-        {
-            printf("Character Device\n");
-        }
-        else if(S_ISDIR(sobj.st_mode))
-        {
-            printf("Directory File\n");
-        }
-        else if(S_ISREG(sobj.st_mode))
-        {
-            printf("Regular file\n");
-        }
-        else if(S_ISSOCK(sobj.st_mode))
-        {
-            printf("Socket file\n");
-        }
-        else if(S_ISFIFO(sobj.st_mode))
-        {
-            printf("Pipe file\n");
-        }
-        else if(S_ISLNK(sobj.st_mode))
-        {
-            printf("Symbolic link\n");
-        }       
+        printf("Character Device\n");
     }
+    else if(S_ISDIR(sobj.st_mode))
+    {
+        printf("Directory File\n");
+    }
+    else if(S_ISREG(sobj.st_mode))
+    {
+        printf("Regular file\n");
+    }
+    else if(S_ISSOCK(sobj.st_mode))
+    {
+        printf("Socket file\n");
+    }
+    else if(S_ISFIFO(sobj.st_mode))
+    {
+        printf("Pipe file\n");
+    }
+    else if(S_ISLNK(sobj.st_mode))
+    {
+        printf("Symbolic link\n");
+    }       
 
     return 0;
 }
